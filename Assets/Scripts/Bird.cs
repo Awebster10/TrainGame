3f28b6cb72872544a4f243b0011657a1f1566e53
Alfie Webster <94174590+Awebster10@users.noreply.github.com>
2025-02-23 02:25:40 +0000

    Added the bird :)

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Windows.Input;
+using TMPro;
 
 public class Bird : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject DeathMessage;
+    public GameObject InteractMessage;
+    public TextMeshProUGUI Message;
+    public bool AtBird = false;
+    public bool IsBirdDead = false;
+
+    public Animator animator;
+
+    public Cherry cherry;
+    public Death death;
+
+    void OnTriggerStay2D(Collider2D collision)
     {
-        
+        InteractMessage.SetActive(true);
+        AtBird = true;
     }
 
-    // Update is called once per frame
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        InteractMessage.SetActive(false);
+        AtBird = false;
+    }
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (AtBird == true)
+            {
+                if (cherry.HasCherry == false)
+                {
+                    DeathMessage.SetActive(true);
+                    death.IsDead = true;
+                }
+                else
+                {
+                    IsBirdDead = true;
+                    Message.text = ("Press E to pick up Bird Cage");
+                    animator.SetBool("BirdDead", true);
+                }
+            }
+        }
     }
 }
